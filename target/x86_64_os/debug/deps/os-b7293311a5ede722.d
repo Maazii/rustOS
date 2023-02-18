ecfbc793581ed27cd2c09c11ff668e1e62af3c3a
Maaz ur Rehman Shah <maaz_rehman39@hotmail.com>
2023-02-18 13:18:43 +0500

    implementing functionalities that access the vga

--- target/x86_64_os/debug/deps/os-b7293311a5ede722.d
+++ target/x86_64_os/debug/deps/os-b7293311a5ede722.d
@@ -1,5 +1,6 @@
-D:\os\target\x86_64_os\debug\deps\os-b7293311a5ede722: src\main.rs
+D:\os\target\x86_64_os\debug\deps\os-b7293311a5ede722: src\main.rs src\vga_buffer.rs
 
-D:\os\target\x86_64_os\debug\deps\os-b7293311a5ede722.d: src\main.rs
+D:\os\target\x86_64_os\debug\deps\os-b7293311a5ede722.d: src\main.rs src\vga_buffer.rs
 
 src\main.rs:
+src\vga_buffer.rs:
--- target/x86_64_os/debug/os.d
+++ target/x86_64_os/debug/os.d
@@ -1,1 +1,1 @@
-D:\os\target\x86_64_os\debug\os: D:\os\src\main.rs
+D:\os\target\x86_64_os\debug\os: D:\os\src\main.rs D:\os\src\vga_buffer.rs
